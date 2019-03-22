db370a07821a7cc77ee2556893fac25f96c8df21
chl9955 <sanggon.choi.dc@gmail.com>
2019-03-22 13:38:13 -0400

    created pac_shaper (from pac_shifter since there is no shifting involved.

--- alu.v
+++ alu.v
@@ -2,16 +2,18 @@ module score_alu(score, alu_select, clk, reset_n, enable);
 	
 	input clk,reset_n,enable;
 	input [1:0] alu_select;
-	reg score;
-	output reg [23:0] score;
+	reg score_reg;
+	output [23:0] score;
+	
+	assign score = score_reg;
 	
 	always @(posedge clk) begin
 		if(reset_n) score <= 0;
 		else if(enable) begin
-			else if(alu_select == 2'b00) score <= score + 24'd1;
-			else if(alu_select == 2'b01) score <= score + 24'd5;
-			else if(alu_select == 2'b10) score <= score + 24'd10;
-			else if(alu_select == 2'b11) score <= score <<< 24'd1;
+			else if(alu_select == 2'b00) score_reg <= score_reg + 24'd1;
+			else if(alu_select == 2'b01) score_reg <= score_reg + 24'd5;
+			else if(alu_select == 2'b10) score_reg <= score_reg + 24'd10;
+			else if(alu_select == 2'b11) score_reg <= score_reg <<< 24'd1;
 		end
 	end
 	
--- control_pacman.v
+++ control_pacman.v
@@ -5,17 +5,17 @@ module control_pacman(go, shape, x_out, y_out, clock, reset_n, dir_in);
 	
 	output go;
 	
+	output reg [7:0] x_out;
+	output reg [6:0] y_out;
+	
+	output [24:0] shape;
+	
 	assign go = clock;
 	
 	reg [2:0] current_state,next_state;
 	
 	reg [7:0] x;
 	reg [6:0] y;
-	
-	output reg [7:0] x_out;
-	output reg [6:0] y_out;
-	
-	output [24:0] shape;
 	
 	localparam WAIT = 3'b100, RIGHT = 3'b000, UP = 3'b001, LEFT = 3'b010, DOWN = 3'b011;
 	
@@ -60,7 +60,7 @@ module control_pacman(go, shape, x_out, y_out, clock, reset_n, dir_in);
 	end
 	
 	always @(posedge clock) begin
-		//Change reset value to starting point?
+		//Change reset value to starting point
 		if(!reset_n) begin
 			x <= 8'd0;
 			y <= 7'd0;
@@ -90,55 +90,6 @@ module control_pacman(go, shape, x_out, y_out, clock, reset_n, dir_in);
 		else current_state = next_state;
 	end
 	
-	reg [24:0] next_ani;
-	
-	localparam rightA = 25'b0111011111110001111101110,
-              rightB = 25'b0111011100110001110001110,
-				  upA = 25'b0101011011110111111101110,
-				  upB = 25'b0000010001110111111101110,
-				  leftA = 25'b0111011111000111111101110,
-				  leftB = 25'b0111000111000110011101110,
-				  downA = 25'b0111011111110111101101010,
-				  downB = 25'b0111011111110111000100000;
-				  
-	reg [24:0] next_shape;
-  
-	assign shape = next_ani;
-
-  always @(posedge clock) begin
-    if(!reset_n) begin
-		next_ani <= rightA;
-    end
-    else begin
-		  case(dir_in)
-			RIGHT: begin
-				if(next_ani == upA || next_ani == leftA || next_ani == downA) next_ani <= rightB;
-				else if(next_ani == upB || next_ani == leftB || next_ani == downB) next_ani <= rightA;
-				else next_ani <= (next_ani == rightA) ? rightB : rightA;
-			end
-			UP: begin
-				if(next_ani == rightA || next_ani == leftA || next_ani == downA) next_ani <= upB;
-				else if(next_ani == rightB || next_ani == leftB || next_ani == downB) next_ani <= upA;
-				else next_ani <= (next_ani == upA) ? upB : upA;
-			end
-			LEFT: begin
-				if(next_ani == rightA || next_ani == upA || next_ani == downA) next_ani <= leftB;
-				else if(next_ani == rightB || next_ani == upB || next_ani == downB) next_ani <= leftA;
-				else next_ani <= (next_ani == leftA) ? leftB : leftA;
-			end
-			DOWN: begin
-				if(next_ani == rightA || next_ani == leftA || next_ani == upA) next_ani <= downB;
-				else if(next_ani == rightB || next_ani == leftB || next_ani == upB) next_ani <= downA;
-				else next_ani <= (next_ani == downA) ? downB : downA;
-			end
-			WAIT: begin
-				if(next_ani == rightA || next_ani == rightB) next_ani <= (next_ani == rightA) ? rightB : rightA;
-				else if(next_ani == upA || next_ani == upB) next_ani <= (next_ani == upA) ? upB : upA;
-				else if(next_ani == leftA || next_ani == leftB) next_ani <= (next_ani == leftA) ? leftB : leftA;
-				else next_ani <= (next_ani == downA) ? downB : downA;
-			end
-		 endcase
-    end
-  end
+	pac_shaper p0(.shape(shape), .dir_in(dir_in), .clock(clock), .reset_n(reset_n));
 	
 endmodule
