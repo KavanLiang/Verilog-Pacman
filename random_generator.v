c3b7796b678d37004088cc74b572415c9d44efae
Kavan Liang <kavan.liang@mail.utoronto.ca>
2019-03-27 11:26:13 -0400

    made 8-bit random generator

--- random_generator.v
+++ random_generator.v
@@ -1,26 +1,26 @@
 module random_generator(clk, enable, reset_n, q);
   input clk, enable;
-  output q;
+  output [7:0] q;
   input reset_n;
 
-  reg [4:0] counter;
-  reg [4:0] seed;
-  reg [4:0] outNext;
-  reg [4:0] out;
+  reg [7:0] counter;
+  reg [7:0] seed;
+  reg [7:0] outNext;
+  reg [7:0] out;
 
   assign q = out;
 
 
   always @(posedge clk) begin
     if(!reset_n) begin
-      seed <= 5'b10101;
+      seed <= 8'b10101010;
       counter <= 0;
     end
     else if(enable) begin
       seed <= counter;
     end
     else begin
-      counter <= counter == 5'b11111 ? 5'b0 : counter + 1'b1;
+      counter <= counter == 8'b11111111 ? 8'b0 : counter + 1'b1;
     end
   end
 
@@ -29,17 +29,19 @@ module random_generator(clk, enable, reset_n, q);
       outNext <= seed;
     end
     else begin
-      outNext[4] <= out[4] ^ out[1];
-      outNext[3] <= out[3] ^ out[0];
+      outNext[7] <= out[7] ^ out[1];
+      outNext[6] <= out[6] ^ out[0];
+      outNext[5] <= out[5] ^ outNext[7];
+      outNext[4] <= out[4] ^ outNext[6];
+      outNext[3] <= out[3] ^ outNext[5];
       outNext[2] <= out[2] ^ outNext[4];
       outNext[1] <= out[1] ^ outNext[3];
-      outNext[0] <= out[0] ^ outNext[2];
     end
   end
 
   always @(posedge clk) begin
     if(!reset_n) begin
-        out <=  5'b10101;
+        out <=  8'b10101010;
     end
     else begin
         out <= outNext;
