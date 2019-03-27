9ee0ad6babd630af0bcaf592eb7976e08ebc0901
Kavan Liang <kavan.liang@mail.utoronto.ca>
2019-03-27 10:01:58 -0400

    random generator should work now

--- random_generator.v
+++ random_generator.v
@@ -1,10 +1,15 @@
-module random_generator(clk, enable, reset_n, out);
+module random_generator(clk, enable, reset_n, q);
   input clk, enable;
-  output reg [4:0] out;
+  output q;
+  input reset_n;
 
   reg [4:0] counter;
   reg [4:0] seed;
-  reg [4:0] nextOut;
+  reg [4:0] outNext;
+  reg [4:0] out;
+
+  assign q = out;
+
 
   always @(posedge clk) begin
     if(!reset_n) begin
@@ -14,8 +19,8 @@ module random_generator(clk, enable, reset_n, out);
     else if(enable) begin
       seed <= counter;
     end
-    else
-      counter <= counter + 1'b1;
+    else begin
+      counter <= counter == 5'b11111 ? 5'b0 : counter + 1'b1;
     end
   end
 
@@ -24,11 +29,20 @@ module random_generator(clk, enable, reset_n, out);
       outNext <= seed;
     end
     else begin
-      outNext[4] <= outNext[4] ^ out[1];
-      outNext[3] <= outNext[3] ^ out[0];
-      outNext[2] <= outNext[2] ^ outNext[4];
-      outNext[1] <= outNext[1] ^ outNext[3];
-      outNext[0] <= outNext[0] ^ outNext[2];
-      out <= outNext;
+      outNext[4] <= out[4] ^ out[1];
+      outNext[3] <= out[3] ^ out[0];
+      outNext[2] <= out[2] ^ outNext[4];
+      outNext[1] <= out[1] ^ outNext[3];
+      outNext[0] <= out[0] ^ outNext[2];
+    end
+  end
+
+  always @(posedge clk) begin
+    if(!reset_n) begin
+        out <=  5'b10101;
+    end
+    else begin
+        out <= outNext;
+    end
   end
 endmodule
